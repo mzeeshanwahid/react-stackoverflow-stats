22a6af440754c0d7db6a9501f1be6df906fadb33
Wahid, Muhammad Zeeshan <zeeshan.wahid@afiniti.com>
2020-03-16 12:35:31 +0500

    Dev Dependencies

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 
 const TestComponent: React.FC = () => {
-    const [show, setShow] = useState<Boolean>(false);
+  const [show, setShow] = useState<Boolean>(false);
 
-    return show ? <p>Show: true</p> : <p>Show: false</p>;
-}
+  return (
+    <>
+      {show ? <p>Show: true</p> : <p>Show: false</p>}
+      <button style={{ margin: "30px" }} onClick={e => setShow(!show)}>
+        Click Me
+      </button>
+    </>
+  );
+};
 
 export default TestComponent;
